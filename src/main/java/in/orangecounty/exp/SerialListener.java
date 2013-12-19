5a0bede17fe90c059374b183e85722f1aab2f9a6
Thomas Ramapuram <thomas.ramapuram1@orangecounty.in>
2013-12-19 17:40:36 +0530

    Made Some Changes.

--- src/main/java/in/orangecounty/exp/SerialListener.java
+++ src/main/java/in/orangecounty/exp/SerialListener.java
@@ -12,13 +12,50 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Arrays;
-import static in.orangecounty.impl.Constants.*;
 
 /**
  * Created by thomas on 19/12/13.
  */
 public class SerialListener implements SerialPortEventListener {
     Logger log = LoggerFactory.getLogger(SerialListener.class);
+    /**
+     * Start of Transmission.
+     */
+    private static final int STX = 2;
+    /**
+     * End of Transmission.
+     */
+    private static final int ETX = 3;
+    /**
+     * PositiveAcknowledge.
+     */
+    private static final int ACK = 6;
+    /**
+     * Negative Acknowlegement.
+     */
+    private static final int NAK = 21;
+    /**
+     * Enquiry.
+     */
+    private static final int ENQ = 5;
+    /**
+     * End of Transmission.
+     */
+    private static final int EOT = 4;
+    /**
+     * Permission to Send Char.
+     */
+    private static final int PSC = 60;
+    /**
+     * Stop Transmission Char.
+     */
+    private static final int STC = 124;
+    /**
+     * Initialize.
+     */
+
+
+
     InputStream inputStream;
     SenderImpl sender;
     StringBuilder buffer = new StringBuilder();
@@ -35,22 +72,82 @@ public class SerialListener implements SerialPortEventListener {
             case(SerialPortEvent.DATA_AVAILABLE):
                 log.debug("Data Available Event Received");
                 try {
-                    int x = inputStream.read();
-                    buffer.append((char)x);
-                    if(x == 6){
-                        sender.ackReceived();
-                    }
-                    log.debug("String:" + buffer.toString());
-                    log.debug("Value:" + Arrays.toString(buffer.toString().getBytes()));
+                    process(inputStream.read());
                 } catch (IOException e) {
                     log.error("IOException : ", e);
                 }
                 break;
-            case(SerialPortEvent.CTS):
-                log.debug("CTS Received");
-                break;
             default:
                 log.debug("Unhandled Event Received");
         }
+    }
+
+    private void process(int b){
+        log.debug("Received int message " + b);
+        buffer.append((char) b);
+        if (buffer.length() > 0 && buffer.charAt(buffer.length() - 2) == ETX) {
+            log.warn("message with BCC" + buffer.toString());
+            log.warn("check bcc:" + checkLrc(buffer.toString()));
+            log.warn("check count:" + checkCount(buffer.toString()));
+            if (checkLrc(buffer.toString()) && checkCount(buffer.toString())) {
+                sender.sendACK();
+                processMessage(buffer.toString());
+            } else {
+                sender.sendNAK();
+            }
+            resetBuffer();
+            return;
+        }
+        switch (b) {
+            case (ACK):
+                log.debug("ACK Case");
+                sender.ackReceived();
+                break;
+            case (NAK):
+                log.warn("NAK Case");
+                sender.nakReceived();
+                break;
+            case (ENQ):
+                log.warn("ENQ Case");
+                sender.sendACK();
+                sender.setReceiving(true);
+                resetBuffer();
+                break;
+            case (EOT):
+                log.debug("EOT Case");
+                sender.setReceiving(false);
+                break;
+            case (PSC):
+                log.debug("PSC Case");
+                if (buffer.toString().equals("16")) {
+                    sender.sendEOT();
+                }
+                break;
+            default:
+                log.debug("Default Case: Doing nothing");
+        }
+
+    }
+
+    private void resetBuffer() {
+        buffer = new StringBuilder();
+    }
+
+    private boolean checkCount(String msg) {
+        return ((msg.length() - 6) == Integer.parseInt(msg.substring(6, 8)));
+    }
+
+    private boolean checkLrc(final String msg) {
+        byte lrc = 0;
+        byte[] byteArray = msg.getBytes();
+        for (int x = 1; x < byteArray.length - 1; x++) {
+            lrc = (byte) (lrc ^ byteArray[x]);
+        }
+        return lrc == byteArray[byteArray.length - 1];
+
+    }
+
+    private void processMessage(String s) {
+        log.debug("Message Received : " + s);
     }
 }
