3c82263ef46ec23b0f4d0fdb77c12bb02803b50c
jamsheer <jamsheer.majeed@orangecounty.in>
2015-03-16 12:25:10 +0530

    Check in and Check Out

--- src/main/java/in/orangecounty/tel/cli/Bootstrap.java
+++ src/main/java/in/orangecounty/tel/cli/Bootstrap.java
@@ -53,11 +53,12 @@ public class Bootstrap {
                 } else if(command.toUpperCase().equals("STATUS")){
                     dataLinkProtocol.sendMessage("1!L7007F  ");
                 } else if(command.toUpperCase().equals("CHECKIN")){
-                    dataLinkProtocol.sendMessage("1!L1634B300         21VASANTH KABINI ");
-                    dataLinkProtocol.sendMessage("1!L21266300   SERVER ROOM    ");
+                    dataLinkProtocol.sendMessage("1!L1634B300         21VASANTHKABINI  ");
+//                    dataLinkProtocol.sendMessage("1!L21266300   SERVERROOM     ");
+                    dataLinkProtocol.sendMessage("1!L21266300   SERVERROOM     ");
                 } else if(command.toUpperCase().equals("CHECKINTWO")){
                     dataLinkProtocol.sendMessage("1!L16111300   ");
-                    dataLinkProtocol.sendMessage("1!L21266300   SERVER ROOM    ");
+                    dataLinkProtocol.sendMessage("1!L21266300   SERVERROOM     ");
                 } else if(command.toUpperCase().equals("CHECKOUT")){
                     dataLinkProtocol.sendMessage("1!L16112300   ");
                 } else if(command.toUpperCase().equals("GETDATA")){
