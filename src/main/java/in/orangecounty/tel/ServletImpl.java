a5596667b88a7b59602f4fbf861695062f01b998
jamsheer <jamsheer.majeed@orangecounty.in>
2015-03-30 17:36:20 +0530

    Testing Code

--- src/main/java/in/orangecounty/tel/ServletImpl.java
+++ src/main/java/in/orangecounty/tel/ServletImpl.java
@@ -15,7 +15,7 @@ public class ServletImpl extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html>");
         out.println("<body>");
-        out.println("<h1>Hello Servlet Get</h1>");
+        out.println("<h1>List Extensions</h1>");
         out.println("</body>");
         out.println("</html>");
     }
